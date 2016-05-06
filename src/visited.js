07d0f414f02c82c513fe19cb07b78907b49e4cdc
Andrew Douglass <andrew@dou.glass>
2016-05-06 15:58:09 -0500

    Allow for classes.

--- src/visited.js
+++ src/visited.js
@@ -2,8 +2,9 @@
  * jquery.visited.js
  *
  * Inspired by: http://joelcalifa.com/blog/revisiting-visited
- *
- * @version 1.0.0
+ * @version 1.1.0
+ * @author Andrew Douglass <andrew@dou.glass>
+ * @license MIT
  */
 
 (function($) {
@@ -13,16 +14,29 @@
 
 
   /**
-	 * Sets data-visited='true' as a data attribute on all selected anchor tags for pages visited by the user.
+	 * Attaches an attribute to anchor tags in order to style visited pages.
+   *
+   * By default, data-visited='true' is added to the anchor tags. If a CSS class
+	 * is passed in as an argument, that will be applied to the anchor tag instead.
+	 *
+	 * @param {string} [className] - A css class to apply to visited links.
 	 */
-  $.fn.visited = function() {
+  $.fn.visited = function(className) {
+    className = className || false;
+
     $(this).each(function(index, element) {
       // Checks that it's an internal link, then checks localStorage for item's existence
       if ( element.host == window.location.host
       &&   localStorage.getItem('visited:' + element.pathname)
       ||   localStorage.getItem('visited:' + element.pathname + '/') ) {
-        // can't use $().data, because it doesn't actually update the html
-        $(element).attr('data-visited', true);
+
+        if ( className ) {
+          $(element).addClass(className);
+        } else {
+          // can't use $().data, because it doesn't actually update the html
+          $(element).attr('data-visited', true);
+        }
+
       }
     });
   };
